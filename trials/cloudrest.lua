53f3a43589dc2459c87767f402d671de6ad836b6
Kyzderp <amurtiger224@gmail.com>
2021-09-06 20:43:34 -0700

    Add spears and dunk indicator

--- trials/cloudrest.lua
+++ trials/cloudrest.lua
@@ -3,6 +3,9 @@ local Crutch = CrutchAlerts
 
 ---------------------------------------------------------------------
 local amuletSmashed = false
+local spearsRevealed = 0
+local spearsSent = 0
+local orbsDunked = 0
 
 ---------------------------------------------------------------------
 -- PLAYER STATE
@@ -23,6 +26,9 @@ local function OnCombatStateChanged(_, inCombat)
     -- Reset
     if (not inCombat) then
         amuletSmashed = false
+        spearsRevealed = 0
+        spearsSent = 0
+        Crutch.UpdateSpearsDisplay()
     end
 end
 
@@ -61,23 +67,75 @@ end
 ---------------------------------------------------------------------
 -- SPEARS
 ---------------------------------------------------------------------
-local spearsRevealed = 0
-local spearsSent = 0
 
 -- EVENT_COMBAT_EVENT (number eventCode, number ActionResult result, boolean isError, string abilityName, number abilityGraphic, number ActionSlotType abilityActionSlotType, string sourceName, number CombatUnitType sourceType, string targetName, number CombatUnitType targetType, number hitValue, number CombatMechanicType powerType, number DamageType damageType, boolean log, number sourceUnitId, number targetUnitId, number abilityId, number overflow)
 local function OnOlorimeSpears(_, result, _, _, _, _, sourceName, sourceType, targetName, _, hitValue, _, _, _, sourceUnitId, targetUnitId, abilityId)
     if (abilityId == 104019) then
         -- Spear has appeared
-        d("|c00FF00SPEAR APPEARED|r")
         spearsRevealed = spearsRevealed + 1
+        Crutch.UpdateSpearsDisplay()
+        -- TODO: sound effect
 
     elseif (abilityId == 104036) then
         -- Spear has been sent
-        d("|c00FF00SPEAR SENT|r")
         spearsSent = spearsSent + 1
         if (spearsRevealed < spearsSent) then spearsRevealed = spearsSent end
+        Crutch.UpdateSpearsDisplay()
+
+    elseif (abilityId == 104047) then
+        -- Orb has been dunked
+        orbsDunked = orbsDunked + 1
+        Crutch.UpdateSpearsDisplay()
     end
 end
+
+local function UpdateSpearsDisplay()
+    CrutchAlertsCloudrestSpear1:SetHidden(true)
+    CrutchAlertsCloudrestSpear2:SetHidden(true)
+    CrutchAlertsCloudrestSpear3:SetHidden(true)
+    CrutchAlertsCloudrestCheck1:SetHidden(true)
+    CrutchAlertsCloudrestCheck2:SetHidden(true)
+    CrutchAlertsCloudrestCheck3:SetHidden(true)
+
+    if (spearsRevealed == 0) then
+        return
+    end
+    if (spearsRevealed >= 1) then
+        CrutchAlertsCloudrestSpear1:SetHidden(false)
+        if (spearsSent >= 1) then
+            CrutchAlertsCloudrestSpear1:SetDesaturation(1)
+        else
+            CrutchAlertsCloudrestSpear1:SetDesaturation(0)
+        end
+    end
+    if (spearsRevealed >= 2) then
+        CrutchAlertsCloudrestSpear2:SetHidden(false)
+        if (spearsSent >= 2) then
+            CrutchAlertsCloudrestSpear2:SetDesaturation(1)
+        else
+            CrutchAlertsCloudrestSpear2:SetDesaturation(0)
+        end
+    end
+    if (spearsRevealed >= 3) then
+        CrutchAlertsCloudrestSpear3:SetHidden(false)
+        if (spearsSent >= 3) then
+            CrutchAlertsCloudrestSpear3:SetDesaturation(1)
+        else
+            CrutchAlertsCloudrestSpear3:SetDesaturation(0)
+        end
+    end
+
+    if (orbsDunked >= 1) then
+        CrutchAlertsCloudrestCheck1:SetHidden(false)
+    end
+    if (orbsDunked >= 2) then
+        CrutchAlertsCloudrestCheck2:SetHidden(false)
+    end
+    if (orbsDunked >= 3) then
+        CrutchAlertsCloudrestCheck3:SetHidden(false)
+    end
+end
+Crutch.UpdateSpearsDisplay = UpdateSpearsDisplay
 
 ---------------------------------------------------------------------
 -- Register/Unregister
@@ -109,9 +167,22 @@ function Crutch.RegisterCloudrest()
 
     -- Register Welkynar's Light, 1250ms duration on person who sent spear
     EVENT_MANAGER:RegisterForEvent(Crutch.name .. "WelkynarsLight", EVENT_COMBAT_EVENT, OnOlorimeSpears)
-    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "WelkynarsLight", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_NONE)
     EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "WelkynarsLight", EVENT_COMBAT_EVENT, REGISTER_FILTER_COMBAT_RESULT, ACTION_RESULT_EFFECT_GAINED_DURATION)
     EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "WelkynarsLight", EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, 104036) -- hitvalue 1250
+
+    -- Register Shadow Piercer Exit, 500 duration on person who dunked orb
+    EVENT_MANAGER:RegisterForEvent(Crutch.name .. "ShadowPiercerExit", EVENT_COMBAT_EVENT, OnOlorimeSpears)
+    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "ShadowPiercerExit", EVENT_COMBAT_EVENT, REGISTER_FILTER_COMBAT_RESULT, ACTION_RESULT_EFFECT_GAINED_DURATION)
+    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "ShadowPiercerExit", EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, 104047) -- hitvalue 500
+
+    -- Register summoning portal
+    EVENT_MANAGER:RegisterForEvent(Crutch.name .. "ShadowRealmCast", EVENT_COMBAT_EVENT, function()
+        spearsRevealed = 0
+        spearsSent = 0
+        orbsDunked = 0
+        Crutch.UpdateSpearsDisplay()
+    end)
+    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "ShadowRealmCast", EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, 103946)
 end
 
 function Crutch.UnregisterCloudrest()
