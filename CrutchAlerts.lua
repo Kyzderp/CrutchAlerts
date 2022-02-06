76a3df21fa78bd868063c101c3fb294247310c0b
Kyzderp <amurtiger224@gmail.com>
2022-02-06 00:48:14 -0800

    [0.10.0] Version

--- CrutchAlerts.lua
+++ CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "0.9.0"
+Crutch.version = "0.10.0"
 
 Crutch.registered = {
     begin = false,
