3bfe6008441b8242168fb8f27bd45846d004434d
Kyzderp <amurtiger224@gmail.com>
2023-02-28 00:23:54 -0800

    [0.16.0] API bump to Firesong

--- CrutchAlerts.lua
+++ CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "0.15.3"
+Crutch.version = "0.16.0"
 
 Crutch.registered = {
     begin = false,
