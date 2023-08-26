d411a906fcb118b4825c0e309308dce8eaf076b8
Kyzderp <amurtiger224@gmail.com>
2023-08-25 19:22:43 -0700

    [0.18.2] API bump U39

--- CrutchAlerts.lua
+++ CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "0.18.1"
+Crutch.version = "0.18.2"
 
 Crutch.registered = {
     begin = false,
