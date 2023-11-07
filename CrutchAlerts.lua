f94a8733b5c774e3bd5a4ab61e5ababdcb4787f4
Kyzderp <amurtiger224@gmail.com>
2023-11-07 00:13:02 -0800

    [0.19.5] Bump version

--- CrutchAlerts.lua
+++ CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "0.19.4"
+Crutch.version = "0.19.5"
 
 Crutch.registered = {
     begin = false,
