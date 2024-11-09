9f47264ca8e9485155caee35bc8fa4448ac10bf7
Kyzderp <amurtiger224@gmail.com>
2024-11-09 14:15:21 -0800

    [1.6.4-rc2] Package for testing

--- main/CrutchAlerts.lua
+++ main/CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "1.6.4-rc1"
+Crutch.version = "1.6.4-rc2"
 
 Crutch.registered = {
     begin = false,
