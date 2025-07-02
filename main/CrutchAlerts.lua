a14259efe9825c4c03cc55b0a4867aa7664e326e
Kyzderp <amurtiger224@gmail.com>
2025-07-01 23:06:24 -0700

    [1.15.0-beta1] Bump version for user testing

--- main/CrutchAlerts.lua
+++ main/CrutchAlerts.lua
@@ -6,7 +6,7 @@
 CrutchAlerts = CrutchAlerts or {}
 local Crutch = CrutchAlerts
 Crutch.name = "CrutchAlerts"
-Crutch.version = "1.14.1"
+Crutch.version = "1.15.0-beta1"
 
 Crutch.registered = {
     begin = false,
