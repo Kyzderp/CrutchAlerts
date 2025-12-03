c5a96569b498ce4c02a32c81ba3a7be235529335
Kyzderp <amurtiger224@gmail.com>
2025-12-02 23:09:05 -0800

    Only check Chimera

--- zones/trials/SanitysEdge.lua
+++ zones/trials/SanitysEdge.lua
@@ -2,16 +2,7 @@ local Crutch = CrutchAlerts
 local C = Crutch.Constants
 
 --[[
-gryphon room HM
-
-[20:03:38] [Kyzderp's Derps] zoneId=1427 {x = 172091, y = 40350, z = 238068}
-[20:03:44] [Kyzderp's Derps] zoneId=1427 {x = 172123, y = 40350, z = 242163}
-[20:03:48] [Kyzderp's Derps] zoneId=1427 {x = 170049, y = 40350, z = 242334}
-[20:03:51] [Kyzderp's Derps] zoneId=1427 {x = 168007, y = 40350, z = 242182}
-[20:03:57] [Kyzderp's Derps] zoneId=1427 {x = 168000, y = 40350, z = 238103}
-
 wamasu room nonHM
-
 [03:20:16] [Kyzderp's Derps] zoneId=1427 {x = 192112, y = 40350, z = 240132}
 [03:20:21] [Kyzderp's Derps] zoneId=1427 {x = 189867, y = 40350, z = 242334}
 [03:20:24] [Kyzderp's Derps] zoneId=1427 {x = 187670, y = 40350, z = 240136}
@@ -29,11 +20,6 @@ gryphon all
 6{x = 168007, z = 242182}
 7{x = 167843, z = 240125}
 8{x = 168020, z = 238086}
-
-3{x = 172289, y = 40350, z = 240133}
-5{x = 170051, y = 40350, z = 242334}
-7{x = 167843, y = 40350, z = 240125}
-1{x = 170065, y = 40350, z = 237908}
 
 lion all
 1{x = 179984, y = 40350, z = 237903}
@@ -54,8 +40,6 @@ local function DisableChimeraIcons()
     Crutch.DisableIconGroup("SEChimeraHMWamasu")
 end
 
--- TODO: how many oracles on norm?
--- TODO: is it always boss1? maybe better to check boss1 and boss2 for chimera only?
 -- Vet Twelvane: 17464648 Chimera: 46572396
 -- HM Twelvane: 34929296 Chimera: 93144792
 local function OnPortalCommon(changeType, portal)
@@ -72,12 +56,13 @@ local function OnPortalCommon(changeType, portal)
     local iconGroupString = "SEChimera"
     local _, powerMax = GetUnitPower("boss1", COMBAT_MECHANIC_FLAGS_HEALTH)
 
-    if (powerMax == 34929296 or powerMax == 93144792) then
+    -- boss1 is Chimera
+    if (powerMax == 93144792) then
         iconGroupString = iconGroupString .. "HM"
-    elseif (powerMax == 17464648 or powerMax == 46572396) then
+    elseif (powerMax == 46572396) then
         iconGroupString = iconGroupString .. "Vet"
     else
-        -- TODO: norm?
+        -- TODO: norm has the 3 along the back wall
         return
     end
 
