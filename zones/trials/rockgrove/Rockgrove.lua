fbc4c69a1a3758a459d25508f777d354bd56bcc3
Kyzeragon <amurtiger224@gmail.com>
2026-01-21 19:24:10 -0800

    Separate Oax file

--- zones/trials/rockgrove/Rockgrove.lua
+++ zones/trials/rockgrove/Rockgrove.lua
@@ -6,82 +6,6 @@ local PANEL_PORTAL_DIRECTION_INDEX = 1
 local PANEL_PORTAL_COUNT_INDEX = 2
 local PANEL_PORTAL_TIMER_INDEX = 3
 
-local EXIT_LEFT_POOL = {x = 91973, y = 35751, z = 81764}  -- from QRH so that we use the same sorting
-
----------------------------------------------------------------------
--- Sludge icons
----------------------------------------------------------------------
-local SLUDGE_UNIQUE_NAME = "CrutchAlertsRGSludge"
-
-local function OnSludgeIcon(changeType, unitTag)
-    if (changeType == EFFECT_RESULT_GAINED) then
-        Crutch.SetAttachedIconForUnit(unitTag, SLUDGE_UNIQUE_NAME, C.PRIORITY.MECHANIC_1_PRIORITY, "CrutchAlerts/assets/poop.dds", nil, {0.6, 1, 0.6})
-    elseif (changeType == EFFECT_RESULT_FADED) then
-        Crutch.RemoveAttachedIconForUnit(unitTag, SLUDGE_UNIQUE_NAME)
-    end
-end
-
----------------------------------------------------------------------
--- OAXILTSO: NOXIOUS SLUDGE SIDES
----------------------------------------------------------------------
-local sludgeTag1 = nil
-local lastSludge = 0 -- for resetting
-
--- EVENT_EFFECT_CHANGED (number eventCode, MsgEffectResult changeType, number effectSlot, string effectName, string unitTag, number beginTime, number endTime, number stackCount, string iconName, string buffType, BuffEffectType effectType, AbilityType abilityType, StatusEffectType statusEffectType, string unitName, number unitId, number abilityId, CombatUnitType sourceType)
-local function OnNoxiousSludgeGained(_, changeType, _, _, unitTag)
-    if (Crutch.savedOptions.rockgrove.showSludgeIcons) then
-        OnSludgeIcon(changeType, unitTag)
-    end
-
-    if (changeType ~= EFFECT_RESULT_GAINED) then return end
-    Crutch.dbgSpam(string.format("|c00FF00Noxious Sludge: %s (%s)|r", GetUnitDisplayName(unitTag), unitTag))
-
-    if (not Crutch.savedOptions.rockgrove.sludgeSides) then return end
-
-    local currSeconds = GetGameTimeSeconds()
-    if (currSeconds - lastSludge > 10) then
-        -- Reset
-        sludgeTag1 = nil
-        lastSludge = currSeconds
-    end
-
-    if (not sludgeTag1) then
-        sludgeTag1 = unitTag
-        return
-    elseif (sludgeTag1 == unitTag) then
-        return
-    end
-
-    local leftPlayer, rightPlayer
-
-    -- TODO: update this if QRH updates. QRH currently sends whoever is closer to
-    -- exit left pool to the left
-    leftPlayer = sludgeTag1
-    rightPlayer = unitTag
-    local _, p1x, p1y, p1z = GetUnitWorldPosition(sludgeTag1)
-    local _, p2x, p2y, p2z = GetUnitWorldPosition(unitTag)
-
-    -- We have sludgeTag1, and unitTag is second player
-    -- Using the same logic as QRH to sort players
-    -- QRH does this by checking who is closer to exit left pool
-    -- Is problematic because of latency, but oh well
-    local p1Dist = Crutch.GetSquaredDistance(p1x, p1y, p1z, EXIT_LEFT_POOL.x, EXIT_LEFT_POOL.y, EXIT_LEFT_POOL.z)
-    local p2Dist = Crutch.GetSquaredDistance(p2x, p2y, p2z, EXIT_LEFT_POOL.x, EXIT_LEFT_POOL.y, EXIT_LEFT_POOL.z)
-    -- Crutch.dbgOther(string.format("squared dist between: %f", Crutch.GetSquaredDistance(p1x, p1y, p1z, p2x, p2y, p2z)))
-    if (p1Dist < p2Dist) then
-        leftPlayer = sludgeTag1
-        rightPlayer = unitTag
-    else
-        leftPlayer = unitTag
-        rightPlayer = sludgeTag1
-    end
-    -- Crutch.dbgOther(string.format("%f", p1Dist))
-    -- Crutch.dbgOther(string.format("%f", p2Dist))
-    Crutch.dbgOther(GetUnitDisplayName(leftPlayer) .. "< >" .. GetUnitDisplayName(rightPlayer))
-    local label = string.format("|c00FF00%s |c00d60b|t100%%:100%%:Esoui/Art/Buttons/large_leftarrow_up.dds:inheritcolor|t |c00FF00Noxious Sludge|r |c00d60b|t100%%:100%%:Esoui/Art/Buttons/large_rightarrow_up.dds:inheritcolor|t |c00FF00%s|r", GetUnitDisplayName(leftPlayer), GetUnitDisplayName(rightPlayer))
-    Crutch.DisplayNotification(157860, label, 5000, 0, 0, 0, 0, 0, 0, 0, true)
-end
-
 
 ---------------------------------------------------------------------
 -- Bahsei
@@ -536,10 +460,7 @@ function Crutch.RegisterRockgrove()
         Crutch.InfoPanel.RemoveLine(PANEL_PORTAL_DIRECTION_INDEX)
     end)
 
-    -- Register the Noxious Sludge
-    EVENT_MANAGER:RegisterForEvent(Crutch.name .. "NoxiousSludge", EVENT_EFFECT_CHANGED, OnNoxiousSludgeGained)
-    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "NoxiousSludge", EVENT_EFFECT_CHANGED, REGISTER_FILTER_ABILITY_ID, 157860)
-    EVENT_MANAGER:AddFilterForEvent(Crutch.name .. "NoxiousSludge", EVENT_EFFECT_CHANGED, REGISTER_FILTER_UNIT_TAG_PREFIX, "group")
+    Crutch.RegisterRockgroveOax()
 
     -- Register for Bahsei portal effect gained/faded
     EVENT_MANAGER:RegisterForEvent(Crutch.name .. "BitterMarrowEffect", EVENT_EFFECT_CHANGED, OnBitterMarrowChanged)
@@ -606,7 +527,6 @@ end
 
 function Crutch.UnregisterRockgrove()
     -- Clean up in case of PTE; unit tags may change
-    Crutch.RemoveAllAttachedIcons(SLUDGE_UNIQUE_NAME)
     if (playerCurseLinesKey) then
         Crutch.Drawing.RemoveWorldTexture(playerCurseLinesKey)
         playerCurseLinesKey = nil
@@ -616,7 +536,8 @@ function Crutch.UnregisterRockgrove()
 
     Crutch.UnregisterExitedGroupCombatListener("RockgroveExitedCombat")
 
-    EVENT_MANAGER:UnregisterForEvent(Crutch.name .. "NoxiousSludge", EVENT_COMBAT_EVENT)
+    Crutch.UnregisterRockgroveOax()
+    
     EVENT_MANAGER:UnregisterForEvent(Crutch.name .. "BitterMarrowEffect", EVENT_EFFECT_CHANGED)
     EVENT_MANAGER:UnregisterForEvent(Crutch.name .. "KissOfDeath", EVENT_COMBAT_EVENT)
     EVENT_MANAGER:UnregisterForEvent(Crutch.name .. "Bleeding", EVENT_EFFECT_CHANGED)
