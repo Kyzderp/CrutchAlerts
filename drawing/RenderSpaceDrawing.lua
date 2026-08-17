16a71f3c2b88104aac53963308f0de6df84a594d
Kyzderp <amurtiger224@gmail.com>
2026-08-16 20:23:21 -0700

    Move ACACUC to PC

--- drawing/RenderSpaceDrawing.lua
+++ drawing/RenderSpaceDrawing.lua
@@ -45,17 +45,6 @@ Draw.ReleaseRenderSpaceTexture = ReleaseRenderSpaceTexture
 
 
 ---------------------------------------------------------------------
--- Utils
----------------------------------------------------------------------
-function Draw.AreDepthBuffersSupported()
-    if (IsConsoleUI()) then return false end
-    -- TODO: check XBPA
-    -- TODO: check subsampling
-    return true
-end
-
-
----------------------------------------------------------------------
 -- Init 3D render space and stuff idk
 ---------------------------------------------------------------------
 function Draw.InitializeRenderSpace()
