fd5839a4cbfcc9e705d551f6573aafb987698a24
Kyzderp <amurtiger224@gmail.com>
2026-08-22 07:01:46 -0700

    Adjust more initial solo MHK thresholds

--- bosshealthbar/SoloDungeonThresholds.lua
+++ bosshealthbar/SoloDungeonThresholds.lua
@@ -13,70 +13,36 @@ end
 local soloDungeonThresholds = {
 -- Moon Hunter Keep
     [GetBossName(CRUTCH_BHB_JAILER_MELITUS)] = {
-        [80] = "Werewolves", -- TODO
-        [50] = "Werewolves", -- TODO
-        [30] = "Werewolves", -- TODO
+        [80] = "Werewolves",
+        [50] = "Werewolves",
+        [30] = "Werewolves",
     },
     [GetBossName(CRUTCH_BHB_HEDGE_MAZE_GUARDIAN)] = {
-        [80] = "2 Spriggans", -- maybe? heals to 75
+        [80] = "2 Spriggans", -- heals to 75
         [70] = "Adds",
-        [45] = "3-5 Spriggans", -- maybe? heals to 49. 5? might be because pushed farther?
+        [45] = "3-5 Spriggans", -- heals to 49. sometimes spawns 5, bug?
         [35] = "Adds",
-        [15] = "Adds + 5 Spriggans?",
+        [15] = "Adds + 5 Spriggans",
     },
     [GetBossName(CRUTCH_BHB_MYLENNE_MOONCALLER)] = {
-        [80] = "Warden", -- TODO
-        [60] = "Warden", -- TODO
-        [40] = "Warden", -- TODO
-        [20] = "Warden", -- TODO
+        [80] = "Wardens", 
+        [65] = "Wolves", -- does it also require being after wardens?
+        [50] = "Wardens",
+        [27] = "Wolves", -- TODO: 27? 30? didn't spawn on one, maybe because warden still up?
+        [21] = "Wardens", -- TODO: 21.015 (maybe from healing mm)
     },
     [GetBossName(CRUTCH_BHB_ARCHIVIST_ERNARDE)] = {
-        -- how can the guides be so different??
-        -- xynode: 76, 56, 36
-        -- esoplanet: 80, 60, 40, 20
-        [80] = "Adds", -- TODO
-        [60] = "Adds", -- TODO
-        [40] = "Adds", -- TODO
-        [20] = "Adds", -- TODO
+        [80] = "Symbols",
+        [62] = "Adds", -- TODO: delayed by symbols cast 60 62
+        [50] = "Symbols", -- probably right
+        [40] = "Adds", -- TODO: no idea, burned too fast
+        [30] = "Symbols", -- TODO: no idea, burned too fast
     },
     [GetBossName(CRUTCH_BHB_VYKOSA_THE_ASCENDANT)] = {
-        normHealth = 1515587, -- TODO
-        vetHealth = 4233356, -- TODO
-        hmHealth = 5503363, -- TODO
-        ["Normal"] = {
-            [90] = "Werewolves", -- TODO
-            [80] = "Wolves", -- TODO
-            [70] = "Werewolves", -- TODO
-            [60] = "Wolves", -- TODO
-            [50] = "Werewolves", -- TODO
-            [40] = "Wolves", -- TODO
-            [30] = "Werewolves", -- TODO
-            [20] = "Wolves", -- TODO
-        },
-        ["Veteran"] = {
-            [90] = "Werewolves", -- TODO
-            [80] = "Wolves", -- TODO
-            [70] = "Werewolves", -- TODO
-            [60] = "Wolves", -- TODO
-            [50] = "Werewolves", -- TODO
-            [40] = "Wolves", -- TODO
-            [30] = "Werewolves", -- TODO
-            [20] = "Wolves", -- TODO
-        },
-        ["Hardmode"] = {
-            [90] = "Werewolves", -- TODO
-            [85] = "Werewolves", -- TODO
-            [80] = "Wolves", -- TODO
-            [70] = "Werewolves", -- TODO
-            [65] = "Werewolves", -- TODO
-            [60] = "Wolves", -- TODO
-            [50] = "Werewolves + Warden", -- TODO
-            [45] = "Werewolves", -- TODO
-            [40] = "Wolves", -- TODO
-            [30] = "Werewolves + Rune", -- TODO
-            [25] = "Werewolves", -- TODO
-            [20] = "Wolves", -- TODO
-        },
+        [80] = "Melee + Werewolf",
+        [60] = "Ranged",
+        [40] = "Melee + Werewolf",
+        [20] = "Ranged",
     },
 
 -- March of Sacrifices (Bloodscent Pass)
